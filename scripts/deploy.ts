fdcb44df652fcce8b8bc1ce9ba3e5a4946b75443
Mona Lisarogue-in-anarchy <nehemiaheje@gmail.com>
2024-02-13 14:59:25 +0100

    initialization of a simple todo app after practicing setter and getter functions and testings

---
+++ scripts/deploy.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
+
+  // const lockedAmount = ethers.parseEther("0.001");
+
+  const SetterGetter = await ethers.deployContract("SetterGetter")
+
+  await SetterGetter.waitForDeployment();
+
+  console.log(
+    `SetterGetter contract deployed to ${SetterGetter.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
